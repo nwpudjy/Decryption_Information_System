a6915f2fdac389107314a67581416ab262bebfcd
dingjunyi <129945511+nwpudjy@users.noreply.github.com>
2024-10-22 23:39:40 +0800

    Add files via upload

---
+++ app/static/js/config.js
@@ -0,0 +1,4 @@
+var ip = "http://10.211.55.3:8080/concert/servlet/web/";
+var imgUrl = ip;
+var wechatM = "http://dsjc.zzwinchance.com/WeChat/login.html?type=m&id=";
+var wechatZ = "http://dsjc.zzwinchance.com/WeChat/login.html?type=z&id=";
